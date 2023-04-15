df086d0cad584da680fa44df66080a1646f3be16
Gr00ze <vincenzop23@gmail.com>
2023-04-15 15:30:59 +0200

    fixed unsafe operations

--- src/main/java/net/fabricmc/paxels/PaxelItem.java
+++ src/main/java/net/fabricmc/paxels/PaxelItem.java
@@ -13,15 +13,12 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.Rarity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -33,7 +30,7 @@ public class PaxelItem extends MiningToolItem {
     TagKey<Block> showelTagKey = BlockTags.SHOVEL_MINEABLE;
 
     protected static final Map<Block, Block> STRIPPED_BLOCKS;
-    protected static final Map<Block, BlockState> PATH_STATES;
+    protected static final HashMap<Block,BlockState> PATH_STATES;
 
     private static final float pickaxeAS=-2.8F,axeAS=-3.2F,shovelAS=-3.0F;
     //private static final float functionAS = (float)Math.pow(Math.E,(pickaxeAS + axeAS + shovelAS)*0.3) - 4;
@@ -73,9 +70,10 @@ public class PaxelItem extends MiningToolItem {
     }
 
     private Optional<BlockState> getStrippedState(BlockState state) {
-        return Optional.ofNullable((Block)STRIPPED_BLOCKS.get(state.getBlock())).map((block) -> {
-            return (BlockState)block.getDefaultState().with(PillarBlock.AXIS, (Direction.Axis)state.get(PillarBlock.AXIS));
-        });
+        return Optional.ofNullable(STRIPPED_BLOCKS
+                .get(state.getBlock()))
+                .map((block) -> block.getDefaultState()
+                        .with(PillarBlock.AXIS, state.get(PillarBlock.AXIS)));
     }
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
@@ -85,12 +83,10 @@ public class PaxelItem extends MiningToolItem {
         PlayerEntity playerEntity = context.getPlayer();
         BlockState blockState = world.getBlockState(blockPos);
         //axe context
+        ItemStack itemStack = context.getStack();
         Optional<BlockState> optional = this.getStrippedState(blockState);
         Optional<BlockState> optional2 = Oxidizable.getDecreasedOxidationState(blockState);
-        Optional<BlockState> optional3 = Optional.ofNullable((Block)((BiMap)HoneycombItem.WAXED_TO_UNWAXED_BLOCKS.get()).get(blockState.getBlock())).map((block) -> {
-            return block.getStateWithProperties(blockState);
-        });
-        ItemStack itemStack = context.getStack();
+        Optional<BlockState> optional3 = Optional.ofNullable((Block)((BiMap<?, ?>)HoneycombItem.WAXED_TO_UNWAXED_BLOCKS.get()).get(blockState.getBlock())).map((block) -> block.getStateWithProperties(blockState));
         Optional<BlockState> optional4 = Optional.empty();
 
         if (optional.isPresent()) {
@@ -111,12 +107,10 @@ public class PaxelItem extends MiningToolItem {
                 Criteria.ITEM_USED_ON_BLOCK.trigger((ServerPlayerEntity)playerEntity, blockPos, itemStack);
             }
 
-            world.setBlockState(blockPos, (BlockState)optional4.get(), 11);
-            world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, (BlockState)optional4.get()));
+            world.setBlockState(blockPos, optional4.get(), 11);
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, optional4.get()));
             if (playerEntity != null) {
-                itemStack.damage(1, playerEntity, (p) -> {
-                    p.sendToolBreakStatus(context.getHand());
-                });
+                itemStack.damage(1, playerEntity, (p) -> p.sendToolBreakStatus(context.getHand()));
             }
 
             return ActionResult.success(world.isClient);
@@ -126,18 +120,18 @@ public class PaxelItem extends MiningToolItem {
             return ActionResult.PASS;
         } else {
 
-            BlockState blockState2 = (BlockState)PATH_STATES.get(blockState.getBlock());
+            BlockState blockState2 = PATH_STATES.get(blockState.getBlock());
             BlockState blockState3 = null;
             if (blockState2 != null && world.getBlockState(blockPos.up()).isAir()) {
                 world.playSound(playerEntity, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
                 blockState3 = blockState2;
-            } else if (blockState.getBlock() instanceof CampfireBlock && (Boolean)blockState.get(CampfireBlock.LIT)) {
+            } else if (blockState.getBlock() instanceof CampfireBlock && blockState.get(CampfireBlock.LIT)) {
                 if (!world.isClient()) {
-                    world.syncWorldEvent((PlayerEntity)null, 1009, blockPos, 0);
+                    world.syncWorldEvent(null, 1009, blockPos, 0);
                 }
 
                 CampfireBlock.extinguish(context.getPlayer(), world, blockPos, blockState);
-                blockState3 = (BlockState)blockState.with(CampfireBlock.LIT, false);
+                blockState3 = blockState.with(CampfireBlock.LIT, false);
             }
 
             if (blockState3 != null) {
@@ -145,9 +139,7 @@ public class PaxelItem extends MiningToolItem {
                     world.setBlockState(blockPos, blockState3, 11);
                     world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, blockState3));
                     if (playerEntity != null) {
-                        context.getStack().damage(1, playerEntity, (p) -> {
-                            p.sendToolBreakStatus(context.getHand());
-                        });
+                        context.getStack().damage(1, playerEntity, (p) -> p.sendToolBreakStatus(context.getHand()));
                     }
                 }
 
@@ -158,7 +150,36 @@ public class PaxelItem extends MiningToolItem {
         }
     }
     static {
-        PATH_STATES = Maps.newHashMap((new ImmutableMap.Builder()).put(Blocks.GRASS_BLOCK, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.DIRT, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.PODZOL, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.COARSE_DIRT, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.MYCELIUM, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.ROOTED_DIRT, Blocks.DIRT_PATH.getDefaultState()).build());
-        STRIPPED_BLOCKS = (new ImmutableMap.Builder()).put(Blocks.OAK_WOOD, Blocks.STRIPPED_OAK_WOOD).put(Blocks.OAK_LOG, Blocks.STRIPPED_OAK_LOG).put(Blocks.DARK_OAK_WOOD, Blocks.STRIPPED_DARK_OAK_WOOD).put(Blocks.DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG).put(Blocks.ACACIA_WOOD, Blocks.STRIPPED_ACACIA_WOOD).put(Blocks.ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG).put(Blocks.BIRCH_WOOD, Blocks.STRIPPED_BIRCH_WOOD).put(Blocks.BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG).put(Blocks.JUNGLE_WOOD, Blocks.STRIPPED_JUNGLE_WOOD).put(Blocks.JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG).put(Blocks.SPRUCE_WOOD, Blocks.STRIPPED_SPRUCE_WOOD).put(Blocks.SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG).put(Blocks.WARPED_STEM, Blocks.STRIPPED_WARPED_STEM).put(Blocks.WARPED_HYPHAE, Blocks.STRIPPED_WARPED_HYPHAE).put(Blocks.CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM).put(Blocks.CRIMSON_HYPHAE, Blocks.STRIPPED_CRIMSON_HYPHAE).put(Blocks.MANGROVE_WOOD, Blocks.STRIPPED_MANGROVE_WOOD).put(Blocks.MANGROVE_LOG, Blocks.STRIPPED_MANGROVE_LOG).build();
+
+        PATH_STATES = Maps.newHashMap(new ImmutableMap
+                .Builder<Block,BlockState>()
+                .put(Blocks.GRASS_BLOCK, Blocks.DIRT_PATH.getDefaultState())
+                .put(Blocks.DIRT, Blocks.DIRT_PATH.getDefaultState())
+                .put(Blocks.PODZOL, Blocks.DIRT_PATH.getDefaultState())
+                .put(Blocks.COARSE_DIRT, Blocks.DIRT_PATH.getDefaultState())
+                .put(Blocks.MYCELIUM, Blocks.DIRT_PATH.getDefaultState())
+                .put(Blocks.ROOTED_DIRT, Blocks.DIRT_PATH.getDefaultState())
+                .build());
+        STRIPPED_BLOCKS = (new ImmutableMap
+                .Builder<Block,Block>())
+                .put(Blocks.OAK_WOOD, Blocks.STRIPPED_OAK_WOOD)
+                .put(Blocks.OAK_LOG, Blocks.STRIPPED_OAK_LOG)
+                .put(Blocks.DARK_OAK_WOOD, Blocks.STRIPPED_DARK_OAK_WOOD)
+                .put(Blocks.DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG)
+                .put(Blocks.ACACIA_WOOD, Blocks.STRIPPED_ACACIA_WOOD)
+                .put(Blocks.ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG)
+                .put(Blocks.BIRCH_WOOD, Blocks.STRIPPED_BIRCH_WOOD)
+                .put(Blocks.BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG)
+                .put(Blocks.JUNGLE_WOOD, Blocks.STRIPPED_JUNGLE_WOOD)
+                .put(Blocks.JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG)
+                .put(Blocks.SPRUCE_WOOD, Blocks.STRIPPED_SPRUCE_WOOD)
+                .put(Blocks.SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG)
+                .put(Blocks.WARPED_STEM, Blocks.STRIPPED_WARPED_STEM)
+                .put(Blocks.WARPED_HYPHAE, Blocks.STRIPPED_WARPED_HYPHAE)
+                .put(Blocks.CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM)
+                .put(Blocks.CRIMSON_HYPHAE, Blocks.STRIPPED_CRIMSON_HYPHAE)
+                .put(Blocks.MANGROVE_WOOD, Blocks.STRIPPED_MANGROVE_WOOD)
+                .put(Blocks.MANGROVE_LOG, Blocks.STRIPPED_MANGROVE_LOG)
+                .build();
     }
 }
